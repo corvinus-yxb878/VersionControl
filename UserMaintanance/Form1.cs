fdab3145b32565a68448233fb7304d3f244ffe40
Balázs Zámbó <balazs.zambo@stud.uni-corvinus.hu>
2021-10-08 16:13:34 +0200

    Initial Commit

--- UserMaintanance/Form1.cs
+++ UserMaintanance/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,17 +13,26 @@ namespace UserMaintanance
 {
     public partial class Form1 : Form
     {
+        BindingList<User> users = new BindingList<User>();
         public Form1()
         {
             InitializeComponent();
             label1.Text = Resource1.LastName; // label1
             label2.Text = Resource1.FirstName; // label2
             button1.Text = Resource1.Add; // button1
+            listBox1.DataSource = users;
+            listBox1.ValueMember = "ID";
+            listBox1.DisplayMember = "FullName";
         }
         
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            var u = new User()
+            {
+                LastName = textBox1.Text,
+                FirstName = textBox2.Text
+            };
+            users.Add(u);
         }
     }
 }

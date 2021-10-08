c8a8f667c53325aa36c52ba4a462aa7b829eb4c8
Balázs Zámbó <balazs.zambo@stud.uni-corvinus.hu>
2021-10-08 16:08:40 +0200

    Initial Commit

---
+++ UserMaintanance/Entities/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{public class User
+    {
+        public Guid ID { get; set; } = Guid.NewGuid();
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName
+        {
+            get
+            {
+                return string.Format(
+                    "{0} {1}",
+                    LastName,
+                    FirstName);
+            }
+        }
+
+       
+    }
+}
+
--- UserMaintanance/Form1.cs
+++ UserMaintanance/Form1.cs
@@ -15,6 +15,14 @@ namespace UserMaintanance
         public Form1()
         {
             InitializeComponent();
+            label1.Text = Resource1.LastName; // label1
+            label2.Text = Resource1.FirstName; // label2
+            button1.Text = Resource1.Add; // button1
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
